296fe7209d74cd89eef9a6bfb9379a95ea42cd4c
Luke McKenzie <luke@theclockspot.com>
2021-10-11 00:01:45 -0500

    ibid

--- bot-weekday.py
+++ bot-weekday.py
@@ -13,9 +13,7 @@ POSTCHAN = os.getenv('DISCORD_POSTCHANNELNAME')
 
 #themes, part 1 (see part 2, and also bot.py)
 import themes.default
-import themes.french
-import themes.niccage1
-import themes.niccage2
+import themes.joebob
 import themes.niccage3
 
 client = discord.Client()
@@ -31,17 +29,12 @@ async def on_ready():
         today = date.today()
         #In original usage, I had an array of weekday sets, and we would send the current weekday determined by ISO week % number of sets (last appeared in commit f89d7dd 2021-09-12)
         #themes, part 2 (see part 1)
-        theme = themes.default
-        #if today >= date(2021,10,11): #birthday campout 10/15-17
-          #theme = themes.joebob
-        if today >= date(2021,10,4):
+        if today >= date(2021,10,18):
+          theme = themes.default
+        elif today >= date(2021,10,11): #birthday campout 10/15-17
+          theme = themes.joebob
+        else:
           theme = themes.niccage3
-        elif today >= date(2021,9,27):
-          theme = themes.niccage2
-        elif today >= date(2021,9,20):
-          theme = themes.niccage1
-        elif today >= date(2021,9,13): #ann's bday 9/14
-          theme = themes.french
           
         if theme.weekdays: #if theme specifies weekdays, send the first – any others can come as responses to bot.py
           await channel.send(theme.weekdays[today.weekday()][0])
