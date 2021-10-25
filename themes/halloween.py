d6de0714d599797a0957b8f115295c332dd73787
Luke McKenzie <luke@theclockspot.com>
2021-10-25 00:56:05 -0500

    doot doot

---
+++ themes/halloween.py
@@ -0,0 +1,73 @@
+#halloween (on Sunday)
+
+genericResponses = [
+  "https://cdn.discordapp.com/attachments/855980303570042910/902072367768023050/pumpkinguy.gif",
+  "https://cdn.discordapp.com/attachments/855980303570042910/902072367768023050/pumpkinguy.gif",
+  "https://cdn.discordapp.com/attachments/855980303570042910/902072206262153276/spooky-too-spooky.gif",
+  "https://cdn.discordapp.com/attachments/855980303570042910/902072066658926612/spooky-scary-skeletons-icegif.gif",
+  "https://cdn.discordapp.com/attachments/855980303570042910/902071431142191144/the-nightmare-before-christmas-jack-skellington.gif",
+  "https://cdn.discordapp.com/attachments/855980303570042910/902071380563075132/hi-hey.gif", #skellington
+  "https://cdn.discordapp.com/attachments/855980303570042910/902071310035877888/200-1.gif", #weird singing pumpkins
+  "https://cdn.discordapp.com/attachments/855980303570042910/902071175142834207/200-2.gif", #office parking lot
+  "https://cdn.discordapp.com/attachments/855980303570042910/902071060835483658/tumblr_nsut1aemqs1r3zbhoo2_400.gif", #dooting softly
+  "https://cdn.discordapp.com/attachments/855980303570042910/902071004199788584/skull-skeleton.gif", #doot
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070937044811797/doot-skeleton.gif", #get dooted
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070873756942357/YrGyWp9.gif", #2spooky
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070803628167188/d9f3cbcf4d9e23a316c6db434e9e6551.gif", #doot doot rainbow you need to believe
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070653119758346/51c1389569d7178ca908784944518085.gif", #60s frankenstein
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070587097231420/giphy-7.gif", #cabinet nope
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070517996077096/tumblr_mshjfhfx1P1sx0t3ho1_400.gif", #skeleton embrace
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070462341861406/kill-you-chuckie.gif", #chucky pug
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070333677400064/7a9333084705e6102863dcd3d9d08457.gif", #haters gonna hate
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070285115748382/MadArtisticFairybluebird-size_restricted.gif", #frankenstein i love dead hate living
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070231869063198/giphy-8.gif",
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070124473896970/hocus-pocus-bette-midler.gif",
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070052633853972/hell-yeah.gif",
+  "https://cdn.discordapp.com/attachments/855980303570042910/902071238577504296/giphy-6.gif", #peanuts get into costume
+  "https://cdn.discordapp.com/attachments/855980303570042910/902069753768726568/giphy-10.gif", #peanuts lucy rejects pumpkin
+  "https://cdn.discordapp.com/attachments/855980303570042910/902069640270848081/halloween-charlie-brown.gif", #peanuts russian
+  #"https://cdn.discordapp.com/attachments/855980303570042910/902072263027879986/giphy-2.gif", #light off on
+]
+
+languageResponses = [
+  "https://cdn.discordapp.com/attachments/855980303570042910/902072123739209748/SkeletonGif.gif",
+  "https://cdn.discordapp.com/attachments/855980303570042910/902072123739209748/SkeletonGif.gif",
+  "https://cdn.discordapp.com/attachments/855980303570042910/902071110105985074/200-4.gif", #angry dooting
+  "https://cdn.discordapp.com/attachments/855980303570042910/902071110105985074/200-4.gif", #angry dooting
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070742009675847/halloween-the-office.gif",
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070742009675847/halloween-the-office.gif",
+  "https://cdn.discordapp.com/attachments/855980303570042910/902070937044811797/doot-skeleton.gif",
+  "https://cdn.discordapp.com/attachments/855980303570042910/902069982257635329/halloween-no-thanks.gif", #no thanks
+  "https://cdn.discordapp.com/attachments/855980303570042910/902069888099708948/giphy-9.gif", #cauldron no
+]
+
+whoResponses = False
+
+weekdays = [
+  [ #MONDAY
+    "https://cdn.discordapp.com/attachments/855980303570042910/902068780199473152/halloween-6.gif",
+  ],
+  [ #TUESDAY
+    "https://cdn.discordapp.com/attachments/855980303570042910/902068879818379274/halloween-5.gif",
+  ],
+  [ #WEDNESDAY
+    "https://cdn.discordapp.com/attachments/855980303570042910/902071490806153256/giphy-4.gif", #wednesday
+    "https://cdn.discordapp.com/attachments/855980303570042910/902071817437605908/giphy-11.gif",
+    "https://cdn.discordapp.com/attachments/855980303570042910/902071872370409532/449ae10feed2312f5870011e0e2dbe62.gif",
+    "https://cdn.discordapp.com/attachments/855980303570042910/902071907409596416/giphy-5.gif",
+  ],
+  [ #THURSDAY
+    "https://cdn.discordapp.com/attachments/855980303570042910/902068966917308426/halloween-3.gif",
+  ],
+  [ #FRIDAY
+    "https://cdn.discordapp.com/attachments/855980303570042910/902069061331079188/halloween-2.gif",
+  ],
+  [ #SATURDAY
+    "https://cdn.discordapp.com/attachments/855980303570042910/902069127227772988/halloween-eve-halloween-funny.gif", #tomorrow is halloween nic cage
+    "https://cdn.discordapp.com/attachments/855980303570042910/902069236434894888/giphy-5.gif", #halloween is basically tomorrow
+  ],
+  [ #SUNDAY
+    "https://cdn.discordapp.com/attachments/855980303570042910/902070172641275904/halloween-grumpy-cat.gif",
+    "https://cdn.discordapp.com/attachments/855980303570042910/902069391758352414/200-3.gif", #this is halloween everybody make a scene
+  ]
+]
